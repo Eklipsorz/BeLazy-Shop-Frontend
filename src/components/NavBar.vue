ab1b19adc3209f4dc6c72f711ea7db84d8b3cd06
eklipsorz <eklipsorz@protonmail.ch>
2022-04-02 12:43:01 +0800

    feat: add navbar draft version

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+      BelazyShop
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+</template>
