4171429c6259cca30470c51bb9884a19059b3fe4
eklipsorz <eklipsorz@protonmail.ch>
2022-04-02 14:24:05 +0800

    feat: initialize styles

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,10 +2,17 @@
   <nav class="navbar navbar-light bg-light">
     <a class="navbar-brand" href="#">
       <img src="../assets/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-      BelazyShop
+      BeLazyShop
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+      <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+          <h5 class="text-white h4">Collapsed content</h5>
+          <span class="text-muted">Toggleable via the navbar brand.</span>
+        </div>
+      </div>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
   </nav>
 </template>
